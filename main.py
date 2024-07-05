c39d2f49195668dda01ad77e199979421d4a112c
Jason Erasmus <jason.erasmus@live.co.za>
2024-07-05 22:00:50 +0100

    Updated code and comments

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from alphabets import morse_code_dict
 
-user_message = input("Type your message here: ")
+# Get user message
+user_message = input("Type your message here: ").lower()
 
-
-user_message_lst = list(user_message)
-
+# Initialise empty list to store morse code
 morse_code = []
 
-for letter in user_message_lst:
-    morse_code_dict.get(letter)
-    morse_code.append(morse_code_dict.get(letter))
+# Iterate over characters in the message
+for letter in user_message:
+    # Get the character from the dictionary and convert to morse code
+    #'' handles any unknown characters
+    character = morse_code_dict.get(letter, "")
+    morse_code.append(character)
 
-
+# Joins morse code list into a string with space between each letter
 morse_code_string = " ".join(morse_code)
 
 print(morse_code_string)
